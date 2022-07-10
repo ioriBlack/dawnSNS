a3b4e4c68b0edb63006f1eb023543246733f94b3
iori <havemanyhopes@gmail.com>
2022-07-10 19:16:30 +0900

    belongTop及びhasManyによる各テーブル同士の紐付け

--- app/Follow.php
+++ app/Follow.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Follow extends Model
 {
     //
+     public function post(){
+        return $this->hasMany(App/Post);
+    }
+
+     public function user(){
+        return $this->hasMany(App/User);
+    }
 }
--- app/User.php
+++ app/User.php
@@ -26,4 +26,12 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+         public function post(){
+        return $this->hasMany(App/Post);
+    }
+
+         public function follow(){
+        return $this->hasMany(App/Follow);
+    }
 }
